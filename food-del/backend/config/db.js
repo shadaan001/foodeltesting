f2ed235730810662e4d49d3c4c4e010c4355abe8
shadaan001 <shadaan001@gmail.com>
2026-01-15 19:35:50 +0530

    sooo

--- food-del/backend/config/db.js
+++ food-del/backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://NASREVOLUTION:<db_password>@cluster0.gv8gkoy.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://NASREVOLUTION:nasrevo1234@cluster0.gv8gkoy.mongodb.net/food-del').then(()=>console.log("DB Connected"));
    
 }
 
